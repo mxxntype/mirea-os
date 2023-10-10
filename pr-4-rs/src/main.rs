cfe0254d00aae932878d0fd55e65fd374ffb9cb4
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-10-10 17:37:50 +0300

    refactor: Add `batch_threads()` function, add comments

--- pr-4-rs/src/main.rs
+++ pr-4-rs/src/main.rs
@@ -1,14 +1,12 @@
 use color_eyre::Result;
 use colored::Colorize;
-use std::{
-    sync::Arc,
-    thread::{self},
-    time::Duration,
-};
+use std::{sync::Arc, thread, time::Duration};
 use tokio::sync::Semaphore;
 
+/// Максимальное количество одновременных доступов к семафоре
 const MAX_PERMIT_COUNT: usize = 4;
 
+/// Вариант #2
 #[tokio::main]
 async fn main() -> Result<()> {
     color_eyre::install()?;
@@ -16,60 +14,47 @@ async fn main() -> Result<()> {
     let semaphore: Arc<Semaphore> = Arc::new(Semaphore::new(MAX_PERMIT_COUNT));
     let mut point_count: usize = 0;
 
+    // Создание и присоединения потока A
     create_worker_thread("A".bold().blue(), Arc::clone(&semaphore), 50).await?;
 
-    print_checkpoint(&mut point_count);
+    print_current_point(&mut point_count);
 
-    let thread_j = {
-        let name = "J".bold().magenta();
-        let semaphore = Arc::clone(&semaphore);
-        create_worker_thread(name, semaphore, 450)
-    };
+    // Создание потока J
+    let thread_j = create_worker_thread("J".bold().magenta(), Arc::clone(&semaphore), 450);
 
-    let mut join_handles = vec![];
+    // Создание и присоединения потоков B, C, I
+    batch_threads(
+        vec!["B".bold().red(), "C".bold().yellow(), "I".bold().green()],
+        &semaphore,
+        25,
+        75,
+    )
+    .await?;
+    print_current_point(&mut point_count);
 
-    for i in ["B".bold().red(), "C".bold().yellow(), "I".bold().green()] {
-        thread::sleep(Duration::from_millis(25));
-        let semaphore = Arc::clone(&semaphore);
-        let handle = create_worker_thread(i, semaphore, 75);
-        join_handles.push(handle);
-    }
+    // Создание и присоединения потоков D, E, F
+    batch_threads(
+        vec!["D".bold().blue(), "E".bold().cyan(), "F".bold().magenta()],
+        &semaphore,
+        25,
+        75,
+    )
+    .await?;
+    print_current_point(&mut point_count);
 
-    for handle in join_handles {
-        handle.await?;
-    }
+    // Создание и присоединения потоков G, H
+    batch_threads(
+        vec!["G".bold().red(), "H".bold().yellow()],
+        &semaphore,
+        25,
+        75,
+    )
+    .await?;
 
-    print_checkpoint(&mut point_count);
-
-    join_handles = vec![];
-    for i in ["D".bold().cyan(), "E".bold().blue(), "F".bold().magenta()] {
-        thread::sleep(Duration::from_millis(25));
-        let semaphore = Arc::clone(&semaphore);
-        let handle = create_worker_thread(i, semaphore, 75);
-        join_handles.push(handle);
-    }
-
-    for handle in join_handles {
-        handle.await?;
-    }
-
-    print_checkpoint(&mut point_count);
-
-    join_handles = vec![];
-    for i in ["G".bold().red(), "H".bold().yellow()] {
-        thread::sleep(Duration::from_millis(25));
-        let semaphore = Arc::clone(&semaphore);
-        let handle = create_worker_thread(i, semaphore, 75);
-        join_handles.push(handle);
-    }
-
-    for handle in join_handles {
-        handle.await?;
-    }
-
+    // Присоединение потока J
     thread_j.await?;
 
-    print_checkpoint(&mut point_count);
+    print_current_point(&mut point_count);
 
     println!("Поток {} начался.", "K".bold().blue());
     println!("\n{}", "--- The End ---".bold().italic());
@@ -77,15 +62,20 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
+/// Создать и соединить именованный поток с семафорой
+// Без временной задержки потоки завершаются моментально,
+// из-за чего информационные сообщения выводятся на экран
+// в непредсказуемом порядке.
 fn create_worker_thread(
     name: impl Send + 'static + std::fmt::Display,
     semaphore: Arc<Semaphore>,
     lifetime: u64,
 ) -> tokio::task::JoinHandle<()> {
     tokio::spawn(async move {
-        println!("Поток {name} начался.");
-        let _permit = semaphore.try_acquire().unwrap();
+        println!("Поток {name} начался и ожидает семафор.");
         println!("Поток {name} захватывает семафор.");
+        let _permit = semaphore.try_acquire().unwrap();
+        println!("Поток {name} в семафоре.");
         println!(
             "Переменная семафора равна: {}.",
             MAX_PERMIT_COUNT - semaphore.available_permits()
@@ -95,7 +85,32 @@ fn create_worker_thread(
     })
 }
 
-fn print_checkpoint(count: &mut usize) {
-    *count += 1;
-    println!("\n{}\n", format!("--- Точка {count} ---").bold().italic());
+async fn batch_threads(
+    thread_names: Vec<impl Sync + Send + 'static + std::fmt::Display>,
+    semaphore: &Arc<Semaphore>,
+    spawn_interval: u64,
+    thread_lifetime: u64,
+) -> Result<()> {
+    let mut join_handles = vec![];
+    for i in thread_names {
+        thread::sleep(Duration::from_millis(spawn_interval));
+        let semaphore = Arc::clone(semaphore);
+        let handle = create_worker_thread(i, semaphore, thread_lifetime);
+        join_handles.push(handle);
+    }
+
+    // Присоединение потоков B, C, I
+    for handle in join_handles {
+        handle.await?;
+    }
+    Ok(())
+}
+
+/// Визуализация точек
+fn print_current_point(point_count: &mut usize) {
+    *point_count += 1;
+    println!(
+        "\n{}\n",
+        format!("--- Точка {point_count} ---").bold().italic()
+    );
 }
