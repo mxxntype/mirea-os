359cdec3fb0739f433c1c28f41ed6f4f32ff460b
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-11-01 13:42:52 +0300

    refactor: Move `process::test` to `page::tests`

--- pr-5-rs/src/process.rs
+++ pr-5-rs/src/process.rs
@@ -1,5 +1,3 @@
-use crate::page::Page;
-use color_eyre::Result;
 use colored::Colorize;
 use rand::{self, Rng};
 use std::fmt;
@@ -32,45 +30,6 @@ impl Process {
     }
 }
 
-impl Page {
-    /// Load a [`Process`] into memory.
-    #[allow(dead_code)]
-    pub fn load_process(&mut self, process: &Process) -> Result<()> {
-        eyre::ensure!(
-            self.loaded_processes < crate::page::PAGE_SIZE / PROCESS_SIZE,
-            "Not enough space in page to write another process"
-        );
-        let start: usize = self.loaded_processes * PROCESS_SIZE;
-        for i in 0..PROCESS_SIZE {
-            let offset = i + start;
-            *self.bytes[offset].try_borrow_mut()? = process.instructions[i];
-        }
-        self.map.insert(process.pid, start);
-        self.loaded_processes += 1;
-        Ok(())
-    }
-
-    /// Unload a [`Process`] from memory.
-    #[allow(dead_code)]
-    pub fn unload_process(&mut self, pid: u16) -> Result<Process> {
-        let offset = self.map.remove(&pid).ok_or(eyre::eyre!(
-            "Cannot unload process: process with pid {pid} isn't loaded"
-        ))?;
-        eyre::ensure!(
-            offset + PROCESS_SIZE <= (self.loaded_processes + 1) * PROCESS_SIZE,
-            "Cannot unload process from memory[{offset}-{}] not mapped to any process, only bytes [0-{}] are mapped",
-            offset + PROCESS_SIZE, (self.loaded_processes + 1) * PROCESS_SIZE
-        );
-        let mut process = Process::with_pid(pid);
-        for i in 0..PROCESS_SIZE {
-            process.instructions[i] = *self.bytes[offset].try_borrow()?;
-            *self.bytes[offset].try_borrow_mut()? = 0; // Zero out the memory
-        }
-        self.loaded_processes -= 1;
-        Ok(process)
-    }
-}
-
 impl fmt::Display for Process {
     #[allow(dead_code)]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -84,38 +43,5 @@ impl fmt::Display for Process {
             write!(f, "{formatted_byte} ")?;
         }
         Ok(())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::Process;
-    use crate::{page::Page, ram::Ram};
-    use color_eyre::Result;
-    use std::rc::Rc;
-
-    #[test]
-    fn load_unload_match() -> Result<()> {
-        const PID2: u16 = 123;
-        let ram = Rc::new(Ram::new());
-        let mut page = Page::new(0, &ram);
-        let first_process = Process::new();
-        let second_process = Process::with_pid(PID2);
-        page.load_process(&first_process)?;
-        page.load_process(&second_process)?;
-        assert_eq!(
-            first_process.pid,
-            page.unload_process(first_process.pid)?.pid
-        );
-        assert_eq!(PID2, page.unload_process(PID2)?.pid);
-        Ok(())
-    }
-
-    #[test]
-    #[should_panic]
-    fn incorrect_unload() {
-        let ram = Rc::new(Ram::new());
-        let mut page = Page::new(0, &ram);
-        let _ = page.unload_process(321).unwrap();
     }
 }
